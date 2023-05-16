1054f4331689ef92e84c9009e01e1a6feac77187
nguyen duc anh <catandfocus@gmail.com>
2023-05-16 15:57:11 +0700

    Tinh ngay den sn

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,10 +10,40 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello");
-       
-
+            // NameAndAge();
+            calDaysBeforeBD();
             Console.ReadKey();
+        }
+
+        private static void NameAndAge()
+        {
+            string name;
+            int year;
+            Console.WriteLine("Nhap ten: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Nhap nam sinh: ");
+            year = int.Parse(Console.ReadLine());
+            int age = DateTime.Now.Year - year;
+
+            Console.WriteLine(name + " " + age.ToString());
+        }
+
+        private static void calDaysBeforeBD()
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine("Nhap ngay sinh: ");
+            int day = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap thang sinh: ");
+            int month = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap nam sinh: ");
+            Console.ReadLine();
+            int currentYear = DateTime.Now.Year;
+
+            if (month < now.Month || (month == now.Month && day < now.Day )) currentYear++;
+        
+            DateTime bd = new DateTime(currentYear, month, day);
+            TimeSpan result = bd - now;
+            Console.WriteLine("Con " + result.Days + " ngay toi sn cua ban");            
         }
     }
 }
